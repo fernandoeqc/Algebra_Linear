aa31002538cc2d97a7d6e09801df11637ee60847
Joao-ava <ajoao781238@gmail.com>
2024-03-03 13:52:01 -0300

    Adicionado testes de LinearAlgebra para os metodos transpose e sum

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from algebra import Matrix, Vector
+from algebra import Matrix, Vector, LinearAlgebra
 
 
 class TestMatrix(unittest.TestCase):
@@ -59,6 +59,111 @@ class TestVector(unittest.TestCase):
         self.assertEqual(vector.get(1), 9)
         self.assertEqual(vector.get(3), 2)
 
+
+class TestLinearAlgebra(unittest.TestCase):
+    def test_transpose(self):
+        matrix = Matrix(2, 3, [
+            3, 2, 1,
+            7, 8, 9
+        ])
+        transpose_matrix = LinearAlgebra.transpose(matrix)
+        self.assertEqual(transpose_matrix.rows, 3)
+        self.assertEqual(transpose_matrix.cols, 2)
+        self.assertSequenceEqual(transpose_matrix.elements, [
+            3, 7,
+            2, 8,
+            1, 9
+        ])
+
+    def test_sum_matrix(self):
+        # adição de duas matriz 2x3
+        first = Matrix(2, 3, [
+            1, 2, 1,
+            1, 2, 1
+        ])
+        second = Matrix(2, 3, [
+            3, 2, 1,
+            7, 8, 9
+        ])
+        result = LinearAlgebra.sum(first, second)
+        self.assertEqual(result.rows, 3)
+        self.assertEqual(result.cols, 2)
+        self.assertSequenceEqual(result.elements, [
+            4, 4, 2,
+            8, 10, 10
+        ])
+        
+        # Adição de duas matriz 2x2
+        first = Matrix(2, 2, [
+            2, 2,
+            1, 2,
+        ])
+        second = Matrix(2, 2, [
+            3, 5,
+            6, 3
+        ])
+        result = LinearAlgebra.sum(first, second)
+        self.assertEqual(result.rows, 2)
+        self.assertEqual(result.cols, 2)
+        self.assertSequenceEqual(result.elements, [
+            5, 7,
+            7, 5
+        ])
+        # Erro de soma de matriz com tamanhos diferentes
+        with self.assertRaisesRegex(Exception, 'tamanhos diferente'):
+            first = Matrix(3, 2, [
+                2, 2,
+                1, 2,
+                2, 1
+            ])
+            second = Matrix(2, 2, [
+                3, 5,
+                6, 3
+            ])
+            LinearAlgebra.sum(first, second)
+
+    def test_sum_vector(self):
+        # Soma de vetor
+        first = Vector(3, [
+            1, 2, 1,
+        ])
+        second = Vector(3, [
+            3, 1, 3
+        ])
+        result = LinearAlgebra.sum(first, second)
+        self.assertEqual(result.dim, 3)
+        self.assertEqual(result.elements, [4, 3, 4])
+        # Erro de soma de vetor com tamanhos diferentes
+        with self.assertRaisesRegex(Exception, 'tamanhos diferente'):
+            first = Vector(3, [
+                1, 2, 1,
+            ])
+            second = Vector(2, [
+                3, 1
+            ])
+            LinearAlgebra.sum(first, second)
+
+    def test_sum_matrix_vector(self):
+        with self.assertRaisesRegex(Exception, 'Não é possivel somar vetores e matrizes'):
+            first = Vector(3, [
+                1, 2, 1,
+            ])
+            second = Matrix(2, 2, [
+                3, 5,
+                6, 3
+            ])
+            LinearAlgebra.sum(first, second)
+
+        
+        with self.assertRaisesRegex(Exception, 'Não é possivel somar vetores e matrizes'):
+            first = Matrix(2, 2, [
+                3, 5,
+                6, 3
+            ])
+            second = Vector(3, [
+                1, 2, 1,
+            ])
+            LinearAlgebra.sum(first, second)
 
 if __name__ == '__main__':
     unittest.main()
