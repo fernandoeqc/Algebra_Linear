68ac48bd21be6fb9f6693f99d644037a39df47f6
Joao-ava <ajoao781238@gmail.com>
2024-03-09 14:07:13 -0300

    Codigo da Andressa em transpose e sum

--- algebra.py
+++ algebra.py
@@ -50,15 +50,63 @@ class Matrix:
         if j < 1 or j > self.cols:
             raise Exception('Essa matriz não possui a coluna ' + j)
 
-        self.elements[(i-1) * self.rows + (j-1)] = value
+        self.matrix[i-1][j-1] = value
+
+    @property
+    def elements(self):
+        elements = []
+        for row in self.matrix:
+            for item in row:
+                elements.append(item)
+        return elements
 
 
 class LinearAlgebra:
-    def transpose():
-        pass
+    @staticmethod
+    def transpose(a):
+        """
+        Retorna a matriz ou vetor transposto.
 
-    def sum():
-        pass
+        :param a: A matriz ou vetor a ser transposto.
+        :return: Uma matriz ou vetor transposto.
+        """
+        if isinstance(a, Matrix):
+            # Se 'a' for uma matriz
+            transposed_elements = []
+            for j in range(1, a.cols + 1):
+                column = [a.get(i, j) for i in range(1, a.rows + 1)]
+                transposed_elements.extend(column)
+            return Matrix(a.cols, a.rows, transposed_elements)
+
+        if isinstance(a, Vector):
+            # Se 'a' for um vetor
+            return Vector(a.dim, [a.get(i) for i in range(1, a.dim + 1)])
+        
+        raise ValueError("O parâmetro 'a' deve ser uma matriz ou um vetor.")
+
+    @staticmethod
+    def sum(a, b):
+        """
+        Soma duas matrizes ou dois vetores.
+
+        :param a: A matriz ou vetor a ser somado.
+        :param b: A matriz ou vetor a ser somado.
+        :return: Uma matriz ou vetor resultante da soma.
+        """
+        if isinstance(a, Matrix) and isinstance(b, Matrix):
+            # Se ambos 'a' e 'b' forem matrizes
+            if a.rows != b.rows or a.cols != b.cols:
+                raise ValueError("As matrizes devem ter a mesma dimensão para a soma.")
+            sum_elements = [a.get(i, j) + b.get(i, j) for i in range(1, a.rows + 1) for j in range(1, a.cols + 1)]
+            return Matrix(a.rows, a.cols, sum_elements)
+        if isinstance(a, Vector) and isinstance(b, Vector):
+            # Se ambos 'a' e 'b' forem vetores
+            if a.dim != b.dim:
+                raise ValueError("Os vetores devem ter a mesma dimensão para a soma.")
+            sum_elements = [a.get(i) + b.get(i) for i in range(1, a.dim + 1)]
+            return Vector(a.dim, sum_elements)
+
+        raise ValueError("Os parâmetros 'a' e 'b' devem ser ambos matrizes ou ambos vetores.")
 
     def times():
         pass
