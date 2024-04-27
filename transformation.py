743069f3ce96f56df6757eab8e1f1d4166e6e201
Fernando C <fernandoeqc@live.com>
2024-04-27 10:46:57 -0300

    Update cenario 2

---
+++ transformation.py
@@ -0,0 +1,60 @@
+from algebra import *
+
+class Transformations:
+    def cartesiana_to_homogenea() -> Matrix:
+        pass
+
+    def homogenea_to_cartesiana() -> Matrix:
+        pass
+
+    def translate2D() -> Matrix:
+        pass
+
+    def translate3D() -> Matrix:
+        pass
+
+    def roration2D() -> Matrix:
+        pass
+
+    def rotation3DX() -> Matrix:
+        pass
+
+    def rotation3DY() -> Matrix:
+        pass
+
+    def rotation3DZ() -> Matrix:
+        pass
+
+    def reflection2DX() -> Matrix:
+        pass
+
+    def reflection2DY() -> Matrix:
+        pass
+
+    def reflection3DX() -> Matrix:
+        pass
+
+    def reflection3DY() -> Matrix:
+        pass
+
+    def reflection3DZ() -> Matrix:
+        pass
+
+    def projection2DX() -> Matrix:
+        pass
+
+    def projection2DY() -> Matrix:
+        pass
+
+    def projection3DX() -> Matrix:
+        pass
+
+    def projection3DY() -> Matrix:
+        pass
+
+    def projection3DZ() -> Matrix:
+        pass
+
+    def shearing() -> Matrix:
+        pass
+
