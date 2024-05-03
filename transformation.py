85bb8551c5816190786d40174384b60e3869f669
Joao-ava <ajoao781238@gmail.com>
2024-05-03 12:15:15 -0300

    change: classmethod to static method

--- transformation.py
+++ transformation.py
@@ -8,125 +8,126 @@ class Transformations:
     def _homogenea_to_cartesiana(self) -> Matrix:
         pass
 
-    def _vector_to_matrix(vector:Vector) -> Matrix:
+    @staticmethod
+    def _vector_to_matrix(vector: Vector) -> Matrix:
        return Matrix(vector.dim, 1, vector.elements)
 
-    @classmethod
-    def translate2D(self, vector:Vector, dx, dy) -> Matrix:
+    @staticmethod
+    def translate2D(vector: Vector, dx, dy) -> Matrix:
         pass
 
-    @classmethod
-    def translate3D(self, vector:Vector, dx, dy, dz) -> Matrix:
+    @staticmethod
+    def translate3D(vector: Vector, dx, dy, dz) -> Matrix:
         pass
 
-    @classmethod
-    def roration2D(self, vector:Vector, angle) -> Matrix:
+    @staticmethod
+    def roration2D(vector: Vector, angle) -> Matrix:
         pass
 
-    @classmethod
-    def rotation3DX(self, vector:Vector, angle) -> Matrix:
+    @staticmethod
+    def rotation3DX(vector: Vector, angle) -> Matrix:
         pass
 
-    def rotation3DY(self, vector:Vector, angle) -> Matrix:
+    def rotation3DY(vector: Vector, angle) -> Matrix:
         pass
 
-    @classmethod
-    def rotation3DZ(self, vector:Vector, angle) -> Matrix:
+    @staticmethod
+    def rotation3DZ(vector: Vector, angle) -> Matrix:
         pass
 
-    @classmethod
-    def reflection2DX(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def reflection2DX(vector: Vector) -> Matrix:
         pass
 
-    @classmethod
-    def reflection2DY(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def reflection2DY(vector: Vector) -> Matrix:
         pass
 
-    @classmethod
-    def reflection3DX(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def reflection3DX(vector: Vector) -> Matrix:
         pass
 
-    @classmethod
-    def reflection3DY(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def reflection3DY(vector: Vector) -> Matrix:
         pass
 
-    @classmethod
-    def reflection3DZ(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def reflection3DZ(vector: Vector) -> Matrix:
         pass
 
-    @classmethod
-    def projection2DX(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def projection2DX(vector: Vector) -> Matrix:
         canonical_matrix = [
             1, 0,
             0, 0
         ]
 
-        cmatrix:Matrix = Matrix(2, 2, canonical_matrix)
-        vectorM = self._vector_to_matrix(vector)
+        cmatrix: Matrix = Matrix(2, 2, canonical_matrix)
+        vectorM = Transformations._vector_to_matrix(vector)
 
         result = LinearAlgebra.dot(cmatrix, vectorM)
 
         return result
 
-    @classmethod
-    def projection2DY(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def projection2DY(vector: Vector) -> Matrix:
         canonical_matrix = [
             0, 0,
             0, 1
         ]
 
-        cmatrix:Matrix = Matrix(2, 2, canonical_matrix)
-        vectorM = self._vector_to_matrix(vector)
+        cmatrix: Matrix = Matrix(2, 2, canonical_matrix)
+        vectorM = Transformations._vector_to_matrix(vector)
 
         result = LinearAlgebra.dot(cmatrix, vectorM)
 
         return result
 
-    @classmethod
-    def projection3DX(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def projection3DX(vector: Vector) -> Matrix:
         canonical_matrix = [
             1, 0, 0,
             0, 1, 0,
             0, 0, 0
         ]
 
-        cmatrix:Matrix = Matrix(3, 3, canonical_matrix)
-        vectorM = self._vector_to_matrix(vector)
+        cmatrix: Matrix = Matrix(3, 3, canonical_matrix)
+        vectorM = Transformations._vector_to_matrix(vector)
 
         result = LinearAlgebra.dot(cmatrix, vectorM)
 
         return result
 
-    @classmethod
-    def projection3DY(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def projection3DY(vector: Vector) -> Matrix:
         canonical_matrix = [
             1, 0, 0,
             0, 0, 0,
             0, 0, 1
         ]
 
-        cmatrix:Matrix = Matrix(3, 3, canonical_matrix)
-        vectorM = self._vector_to_matrix(vector)
+        cmatrix: Matrix = Matrix(3, 3, canonical_matrix)
+        vectorM = Transformations._vector_to_matrix(vector)
 
         result = LinearAlgebra.dot(cmatrix, vectorM)
 
         return result
 
-    @classmethod
-    def projection3DZ(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def projection3DZ(vector: Vector) -> Matrix:
         canonical_matrix = [
             0, 0, 0,
             0, 1, 0,
             0, 0, 1
         ]
 
-        cmatrix:Matrix = Matrix(3, 3, canonical_matrix)
-        vectorM = self._vector_to_matrix(vector)
+        cmatrix: Matrix = Matrix(3, 3, canonical_matrix)
+        vectorM = Transformations._vector_to_matrix(vector)
 
         result = LinearAlgebra.dot(cmatrix, vectorM)
 
         return result
 
-    @classmethod
-    def shearing(self, vector:Vector) -> Matrix:
+    @staticmethod
+    def shearing(vector: Vector) -> Matrix:
         pass
