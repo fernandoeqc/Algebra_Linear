cf7f24e5119d4a197537aa6329a5f7d5ea05361c
Joao-ava <ajoao781238@gmail.com>
2024-06-05 19:50:25 -0300

    Adicionado base do page rank

---
+++ page_rank.py
@@ -0,0 +1,23 @@
+class PagePank:
+    @staticmethod
+    def run(A):
+        '''
+        Faz o calculo do page rank
+        '''
+        pass
+
+    @staticmethod
+    def center_vector(A):
+        '''
+        Calcula o vetor centro da matriz A
+        O vetor centro é a soma de todos os elementos das linhas da matriz A
+        '''
+        pass
+
+    @staticmethod
+    def authority_vector(A):
+        '''
+        Calcula o vetor autoridade da matriz A
+        O vetor autoridade é a soma de todos os elementos das colunas da matriz A
+        '''
+        pass
