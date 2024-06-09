5712f9d02e140e1bc4c85ff57fb0744699d086b8
Joao-ava <ajoao781238@gmail.com>
2024-06-09 10:44:39 -0300

    Adicionado exemplos do livro

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-from app.algebra import Matrix, Vector, LinearAlgebra
+from app.algebra import Matrix
 from app.page_rank import PageRank
 
 
-A = Matrix(4, 4, [
-    0, 0, 1, 1,
-    1, 0, 0, 0,
-    1, 0, 0, 1,
-    1, 1, 1, 0,
+matrix_5 = Matrix(4, 4, [
+    0, 0, 1, 0, # 1
+    1, 0, 0, 0, # 2
+    1, 1, 0, 0, # 3
+    0, 1, 0, 0, # 4
 ])
 
-# A = Matrix(2, 2, [
-#     1, 3,
-#     2, -5
-# ])
+print(matrix_5)
+print('Result')
+print(PageRank.run(matrix_5))
 
-# b = Matrix(2, 1, [5, 2])
+matrix_6 = Matrix(4, 4, [
+    0, 1, 1, 0, # 1
+    0, 0, 1, 0, # 2
+    1, 0, 0, 1, # 3
+    1, 0, 0, 0, # 4
+])
+print(matrix_6)
+print('Result')
+print(PageRank.run(matrix_6))
 
-print(PageRank.run(A))
-# print(LinearAlgebra.dot(A, b))
+matrix_7 = Matrix(5, 5, [
+    0, 1, 1, 1, 0, # 1
+    1, 0, 0, 0, 1, # 2
+    0, 0, 0, 0, 1, # 3
+    0, 1, 0, 0, 0, # 4
+    0, 1, 1, 0, 0, # 5
+])
+print(matrix_7)
+print('Result')
+print(PageRank.run(matrix_7))
+
+matrix_8 = Matrix(10, 10, [
+    0, 1, 1, 0, 1, 1, 0, 0, 0, 1, # 1
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0, # 2
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, # 3
+    0, 1, 1, 0, 0, 1, 1, 0, 0, 1, # 4
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, # 5
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, # 6
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, # 7
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, # 8
+    0, 1, 1, 0, 0, 1, 0, 1, 0, 1, # 9
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, # 10
+])
+print(matrix_8)
+print('Result')
+print(PageRank.run(matrix_8))
+
 
